0ef28a270af288db66bd96289f332e71ef15b497
Roberto Cherubin <roberto.cherubin@255.it>
2023-01-17 13:43:21 +0000

    ADD builded frontend to server

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+const morgan = require('morgan');
+
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
@@ -7,7 +10,15 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+app.use(morgan('combined'));  //Logging
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
